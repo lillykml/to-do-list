b7835de554e531c2207e94e5b6a46dfc74908f2b
lillykml <64362067+lillykml@users.noreply.github.com>
2023-02-11 13:58:39 +0100

    Refactored code

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = ["Buy Groceries", "Go for a run"];
-var workItems = [];
-
-// Ideally I want the button to be able to remove the elements
-/*var buttons = document.querySelectorAll(".delete-btn");
-
-for (let i = 0; i < items.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        items.pop();
-    })
-}*/
-
+const items = ["Buy Groceries", "Go for a run"];
+const workItems = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = { weekday: "long", day: "numeric", month: "long" };
-  day = today.toLocaleDateString("en-US", options);
+  day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
@@ -45,3 +34,15 @@ app.get("/work", function(req, res) {
 app.listen(3000, function () {
   console.log("Server started listening on Port 3000");
 });
+
+
+
+
+// Ideally I want the button to be able to remove the elements
+/*var buttons = document.querySelectorAll(".delete-btn");
+
+for (let i = 0; i < items.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        items.pop();
+    })
+}*/
