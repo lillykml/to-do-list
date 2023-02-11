2827a5960709d53b751bfef8cd4aaaf427c8a522
lillykml <64362067+lillykml@users.noreply.github.com>
2023-02-11 12:55:16 +0100

    Added second list for work

--- app.js
+++ app.js
@@ -7,20 +7,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var items = ["Buy Groceries", "Go for a run"];
+var workItems = [];
+
+// Ideally I want the button to be able to remove the elements
+/*var buttons = document.querySelectorAll(".delete-btn");
+
+for (let i = 0; i < items.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        items.pop();
+    })
+}*/
 
 
 app.get("/", function (req, res) {
   var today = new Date();
   var options = { weekday: "long", day: "numeric", month: "long" };
   day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
     var newItem = req.body.newItem;
-    items.push(newItem);
-    res.redirect("/");
+
+    if (req.body.list === "Work") {
+        workItems.push(newItem);
+        res.redirect("/work");
+    } else {
+        items.push(newItem);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work", newListItems: workItems});
+})
 
 app.listen(3000, function () {
   console.log("Server started listening on Port 3000");
